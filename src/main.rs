4eb1d602114f060e6a37d07f50a494d423271e7a
Hakizimana Fred <hakifred20@gmail.com>
2023-09-18 12:08:22 +0300

    json reader

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused)]
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 
 struct Task {
     id: u32,
@@ -13,10 +14,22 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world")
 }
 
+async fn read_task() -> impl Responder {
+    let data = json!({
+        "message": "well done reading a book",
+    });
+
+    // Respond with JSON
+    HttpResponse::Ok().json(data)
+}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .route("/task", web::get().to(read_task))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
